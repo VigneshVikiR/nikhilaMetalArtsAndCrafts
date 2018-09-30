a9c1ec12f4aff9cde1da0bf90f5c7ed9bb039c66
Vraghuraman <vraghuraman@encoress.com>
2018-09-30 23:08:43 +0530

    added carousel image slider

--- OmNama/src/app/app.module.ts
+++ OmNama/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { NgbdCarouselConfig } from './carousel-config';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    NgbdCarouselConfig
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ OmNama/src/app/carousel-config.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'ngbd-carousel-config',
+  templateUrl: './carousel-config.html',
+  providers: [NgbCarouselConfig]
+})
+export class NgbdCarouselConfig {
+  images = ['../assets/sculpture4..png','../assets/background2.png','../assets/background3.png','../assets/background.png'];
+
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 2000;
+    config.wrap = true;
+    config.keyboard = true;
+    config.pauseOnHover = true;
+  }
+}
